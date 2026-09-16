67ee6e5e1231b4503db15676ef33434aa0fe8982
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-09-16 21:06:12 +0530

    added stack push function and updated the stack display function

--- stack/main.c
+++ stack/main.c
@@ -4,11 +4,29 @@
 #include "stack.h"
 
 void test_stack(){
-    Stack stack = stack_new(18);
+    Stack stack = stack_new(10);
+//    Stack stack = stack_new(11);
+    StackResult stack_result;
 
     assert(stack.top == -1);
-    assert(stack.size == 18);
+    assert(stack.size == 10);
+//    assert(stack.size == 11);
 
+    stack_display(&stack);
+
+    stack_push(&stack, 67, &stack_result);
+    stack_display(&stack);
+
+    stack_push(&stack, 51, &stack_result);
+    stack_push(&stack, 63, &stack_result);
+    stack_push(&stack, 33, &stack_result);
+    stack_push(&stack, 95, &stack_result);
+    stack_push(&stack, 88, &stack_result);
+    stack_push(&stack, 91, &stack_result);
+    stack_push(&stack, 24, &stack_result);
+    stack_push(&stack, 12, &stack_result);
+    stack_push(&stack, 15, &stack_result);
+    stack_push(&stack, 99, &stack_result);
     stack_display(&stack);
 }
 
--- stack/stack.c
+++ stack/stack.c
@@ -9,22 +9,44 @@ Stack stack_new(uint32_t size){
 }
 
 uint32_t stack_full(const Stack *stack);
+
 uint32_t stack_empty(const Stack *stack);
 
-Stack *stack_push(Stack *stack, float data, StackResult *stackResult);
+
+Stack *stack_push(Stack *stack, float data, StackResult *stackResult){
+    if(stack -> top + 1 < stack -> size){
+        stack -> stk[++stack -> top] = data;
+        stackResult -> data = stack -> stk[stack -> top];
+        stackResult -> status = STACK_OK;
+    } else {
+        stackResult -> data = data;
+        stackResult -> status = STACK_FULL;
+    }
+    return stack;
+}
+
 Stack *stack_pop(Stack *stack, StackResult *stackResult);
+
 Stack *stack_peek(const Stack *stack, StackResult *stackResult);
+
 
 void stack_display(const Stack *stack){
     printf("\nSTACK {");
-
+    printf("\n\tsize : %d", stack -> size);
+    printf("\n\ttop  : %d", stack -> top);
+    printf("\n\tstk  : {");
     if(stack -> top != -1){
-        for(int i = 0; i <= stack -> top; i++) {
-            printf("\n\tstack[%d] = %d", i, stack -> stk[i]);
+//        for(int i = 0; i < stack -> size; i++) {
+//            printf("\n\t\tstack[%02d] = %f", i, stack -> stk[i]);
+//        }
+        for(int i = stack -> size - 1; i >= 0; i--) {
+            if(stack -> size -1 >= 10) printf("\n\t\tstack[%02d] = %5.2f", i, stack -> stk[i]);
+            else printf("\n\t\tstack[%d] = %5.2f", i, stack -> stk[i]);
+            if(i == stack -> top) printf(" <-- TOP OF STACK");
         }
     } else {
-        printf("\n\tEMPTY");
+        printf("\n\t\tEMPTY");
     }
-
+    printf("\n\t}");
     printf("\n}");
 }
