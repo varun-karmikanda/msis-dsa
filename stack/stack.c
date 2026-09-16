9480eb5194d30c37388a6727b7f63cebab7886b6
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-09-16 22:09:46 +0530

    added stack pop, peek , is_empty and is_full functions

--- stack/stack.c
+++ stack/stack.c
@@ -8,9 +8,13 @@ Stack stack_new(uint32_t size){
     return stack;
 }
 
-uint32_t stack_full(const Stack *stack);
+uint32_t stack_is_full(const Stack *stack){
+    return stack -> top + 1 == stack -> size;
+}
 
-uint32_t stack_empty(const Stack *stack);
+uint32_t stack_is_empty(const Stack *stack){
+    return stack -> top == -1;
+}
 
 
 Stack *stack_push(Stack *stack, float data, StackResult *stackResult){
@@ -25,12 +29,31 @@ Stack *stack_push(Stack *stack, float data, StackResult *stackResult){
     return stack;
 }
 
-Stack *stack_pop(Stack *stack, StackResult *stackResult);
+Stack *stack_pop(Stack *stack, StackResult *stackResult){
+    stack_display(stack, stackResult);
+    if(stack -> top > -1){
+        stackResult -> data = stack -> stk[stack -> top];
+        stackResult -> status = STACK_OK;
+        --stack -> top;
+    } else {
+        stackResult -> data = RESULT_INVALID;
+        stackResult -> status = STACK_EMPTY;
+    }
+    return stack;
+}
 
-Stack *stack_peek(const Stack *stack, StackResult *stackResult);
+Stack *stack_peek(const Stack *stack, StackResult *stackResult){
+    if(stack -> top > -1){
+        stackResult -> data = stack -> stk[stack -> top];
+        stackResult -> status = STACK_OK;
+    } else {
+        stackResult -> data = RESULT_INVALID;
+        stackResult -> status = STACK_EMPTY;
+    }
+}
 
 
-void stack_display(const Stack *stack){
+void stack_display(const Stack *stack, StackResult *stackResult){
     printf("\nSTACK {");
     printf("\n\tsize : %d", stack -> size);
     printf("\n\ttop  : %d", stack -> top);
@@ -47,6 +70,10 @@ void stack_display(const Stack *stack){
     } else {
         printf("\n\t\tEMPTY");
     }
+    printf("\n\tSTACK_RESULT {");
+    printf("\n\t\tdata   : %5.2f", stackResult-> data);
+    printf("\n\t\tstatus : %d", stackResult -> status);
+    printf("\n\t}");
     printf("\n\t}");
     printf("\n}");
 }
--- stack/stack.h
+++ stack/stack.h
@@ -12,7 +12,7 @@
 
 struct _stack_ {
     uint32_t size;
-    uint32_t top;
+    int32_t top;
     float stk[MAX_DEPTH];
 };
 
@@ -27,13 +27,13 @@ typedef struct _stack_result_ StackResult;
 
 Stack stack_new(uint32_t size);
 
-uint32_t stack_full(const Stack *stack);
-uint32_t stack_empty(const Stack *stack);
+uint32_t stack_is_full(const Stack *stack);
+uint32_t stack_is_empty(const Stack *stack);
 
 Stack *stack_push(Stack *stack, float data, StackResult *stackResult);
 Stack *stack_pop(Stack *stack, StackResult *stackResult);
 Stack *stack_peek(const Stack *stack, StackResult *stackResult);
 
-void stack_display(const Stack *stack);
+void stack_display(const Stack *stack, StackResult *stackResult);
 
 #endif // STACK_H_INCLUDED
